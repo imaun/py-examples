6d3adc172f63a9cf25392f59150f5ffbba855309
imun <imun22@gmail.com>
2023-02-20 17:36:20 +0330

    Add example for 4-main-operations

---
+++ src/beginner/4-main-operations/main_ops.py
@@ -0,0 +1,18 @@
+# 4 main operations
+a = input('Input first num: ')
+b = input('Input second num: ')
+
+num1 = int(a)
+num2 = int(b)
+
+sum = num1 + num2
+print(f'Sum : {sum}')
+
+sub = num1 - num2
+print(f'Substract : {sub}')
+
+mul = num1 * num2
+print(f'Multiply : {mul}')
+
+div = num1 / num2
+print(f'Divide : {div}')
